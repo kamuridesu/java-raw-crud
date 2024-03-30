6eb31d2ea8f613e0c16a2d26ccdc3bec6afcc334
Kamuri Amorim <myk.gata14@gmail.com>
2024-03-29 23:38:28 -0300

    feat: finish request class

--- http-server/Server.java
+++ http-server/Server.java
@@ -20,7 +20,8 @@ public class Server {
         public void handle(HttpExchange t) throws IOException {
             var request = new Request(t);
             System.out.println("Connection from " + t.getRemoteAddress());
-            System.out.println("Request body: " + request.body);
+            System.out.println("Request body: " + request.getBody());
+            System.out.println("Headers: \n" + request.getHeaders() + "\n================");
             var response = "OK";
             t.sendResponseHeaders(200, response.length());
             var os = t.getResponseBody();
@@ -31,9 +32,22 @@ public class Server {
 }
 
 
+class Headers extends com.sun.net.httpserver.Headers {
+    @Override
+    public String toString() {
+        var s = "";
+        for (var key : this.keySet()) {
+            s += key + ": " + this.get(key).get(0) + "\n";
+        }
+        return s;
+    }
+}
+
+
 class Request {
-    HttpExchange raw;
-    String body;
+    private HttpExchange raw;
+    private String body;
+    private Headers headers;
 
     Request(HttpExchange t) {
         this.raw = t;
@@ -48,7 +62,21 @@ class Request {
     }
 
     private void processRequestHeaders() {
-        var headers = this.raw.getRequestHeaders();
-        System.out.println(headers);
+        var headers = new Headers();
+        headers.putAll(this.raw.getRequestHeaders());
+        this.headers = headers;
     }
+
+    String getBody() {
+        return this.body;
+    }
+
+    Headers getHeaders() {
+        return this.headers;
+    }
+
+    HttpExchange getRawRequest() {
+        return this.raw;
+    }
+
 }
